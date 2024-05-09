2e4975c258daba8667cc2d9be863f16a5d41dd08
an3TesT4unGA <strungaanete@gmail.com>
2024-05-09 16:17:15 +0300

    updated portfolio

--- my-portfolio/sketch.js
+++ my-portfolio/sketch.js
@@ -1,3 +1,3 @@
 // Preload an image
-const preloadImage = new Image();
-preloadImage.src = "images/background.jpg"; // Preloads the image in the background
+// const preloadImage = new Image();
+// preloadImage.src = "images/background.jpg"; // Preloads the image in the background
